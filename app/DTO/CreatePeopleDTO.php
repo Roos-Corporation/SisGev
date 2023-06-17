55047783aead3e846d320e6520f26ba1d0b72459
Your Name <you@example.com>
2023-06-17 18:07:31 -0300

    Feat: Create the DTO for Person

---
+++ app/DTO/CreatePeopleDTO.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DTO;
+
+use App\Http\Requests\StoreUpdatePeople;
+
+class CreatePeopleDTO
+{
+    public function __construct(
+        public string $cpf,
+        public string $first_name,
+        public string $last_name,
+        public string $accounting,
+        public string $date_of_birth,
+        public string $gender,
+        public string $status
+    )
+    {}
+
+    public static function makeFromRequest(StoreUpdatePeople $request): self
+    {
+        return new self(
+            $request->cpf,
+            $request->first_name,
+            $request->last_name,
+            $request->accounting,
+            $request->date_of_birth,
+            $request->gender,
+            $request->status == 'on'? 'a' : 'i'
+        );
+    }
+
+}
---
+++ app/DTO/UpdatePeopleDTO.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DTO;
+
+use App\Http\Requests\StoreUpdatePeople;
+
+class UpdatePeopleDTO
+{
+    public function __construct(
+        public string $id,
+        public string $cpf,
+        public string $first_name,
+        public string $last_name,
+        public string $accounting,
+        public string $date_of_birth,
+        public string $gender,
+        public string $status
+    )
+    {}
+
+    public static function makeFromRequest(StoreUpdatePeople $request): self
+    {
+        return new self(
+            $request->id,
+            $request->cpf,
+            $request->first_name,
+            $request->last_name,
+            $request->accounting,
+            $request->date_of_birth,
+            $request->gender,
+            $request->status == 'on'? 'a' : 'i'
+        );
+    }
+
+}
