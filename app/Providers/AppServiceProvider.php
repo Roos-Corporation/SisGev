c9a8af651096f8366e79b38ba4a36f7ce0ffa1dc
Your Name <you@example.com>
2023-06-17 18:25:17 -0300

    Feat: Add in the AppServiceProvider the Person interface and the Eloquent ORM

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\PersonEloquentORM;
+use App\Repositories\PersonRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,6 +14,10 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->bind(
+            PersonRepositoryInterface::class,
+            PersonEloquentORM::class
+        );
     }
 
     /**
