d31b7f267edfc536789460a8c37bd9275bad44b1
Your Name <you@example.com>
2023-06-17 18:03:09 -0300

    feat: Adicionar no Model Person $fillable

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'cpf',
+        'first_name',
+        'last_name',
+        'accounting',
+        'date_of_birth',
+        'gender',
+        'status'
+    ];
 }
