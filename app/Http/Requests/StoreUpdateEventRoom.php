545ad920b56e72c2303ad3ad22999efb3d1b028a
Your Name <you@example.com>
2023-06-17 22:16:57 -0300

    feat: Create The Request to validate the EventRoom crud

---
+++ app/Http/Requests/StoreUpdateEventRoom.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreUpdateEventRoom extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            //
+            'name' => [
+                'required',
+                'min:3',
+                'max:255',
+                'unique:event_rooms'
+            ],
+            'capacity' => [
+                'required',
+                'min:1'
+            ],
+            'location' => [
+                'required',
+                'min:1',
+                'max:255'
+            ],
+            'gender' => [
+                'required'
+            ],
+            'status' => [
+                'required'
+            ],
+
+        ];
+        if ( $this->method() === 'PUT' ||  $this->method() === 'PATCH') {
+            $rules['name'] = [
+                'required',
+                'min:3',
+                'max:255',
+                Rule::unique('event_rooms')->ignore($this->event_room)
+            ];
+        }
+
+        return $rules;
+    }
+}
