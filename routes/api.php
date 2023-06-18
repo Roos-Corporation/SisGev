24668aaf06491e77ca0b1572ad57a402357e4d0f
Your Name <you@example.com>
2023-06-18 09:33:53 -0300

    feat: Add route events

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\CoffeeSpaceController;
 use App\Http\Controllers\Api\EventRoomController;
 use App\Http\Controllers\Api\PersonController;
@@ -25,4 +26,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('/people', PersonController::class);
 Route::apiResource('/event-rooms', EventRoomController::class);
 Route::apiResource('/coffee-spaces', CoffeeSpaceController::class);
+Route::apiResource('/events', EventController::class);
+
 
