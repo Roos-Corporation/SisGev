c882bcfa21e54fb9dff9aeb237dce0a7f8cc5187
Your Name <you@example.com>
2023-06-18 14:01:59 -0300

    feat: Create The Request to validate the EventStageParticipant crud

---
+++ app/Http/Requests/StoreUpdateEventStageParticipant.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreUpdateEventStageParticipant extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            //
+            'event_participant_id' => [
+                'required',
+            ],
+            'event_room_id' => [
+                'required',
+            ],
+            'status' => [
+                'required'
+            ],
+
+        ];
+        if ( $this->method() === 'PUT' ||  $this->method() === 'PATCH') {
+            $rules['event_participant_id'] = [
+                'required',
+                Rule::unique('event_stage_participants')->ignore($this->event_stage_participant)
+            ];
+        }
+
+        return $rules;
+    }
+}
